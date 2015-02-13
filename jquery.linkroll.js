9bd24a2a4fe5ba2bd950a89e8aac10af95105240
Zach Shelton <mail@zachofalltrades.net>
2015-02-13 18:31:27 +0000

    reorg into OO design using prototype

--- jquery.linkroll.js
+++ jquery.linkroll.js
@@ -10,116 +10,84 @@
 * http://www.opensource.org/licenses/mit-license.php
 * http://www.gnu.org/licenses/gpl.html
 *  
+* JSONEditor functionality is (currently) handled in the cloud via 'JSON Blob'
+* https://jsonblob.com/about
+* 
+* JSONProxy functionality is handled in the cloud via NodeJitsu
+* https://jsonp.nodejitsu.com/
 */
 
-(function( $ ) { //begin closure to make internal functions and variables private
+(function( $ ) { //begin closure 
 
-	//see http://jsfiddle.net/zachofalltrades/tcgwkns8
-	var regx = /^(((([^:\/#\?]+:)?(?:(\/\/)((?:(([^:@\/#\?]+)(?:\:([^:@\/#\?]+))?)@)?(([^:\/#\?\]\[]+|\[[^\/\]@#?]+\])(?:\:([0-9]+))?))?)?)?((\/?(?:[^\/\?#]+\/+)*)([^\?#]*)))?(\?[^#]+)?)(#.*)?/;
-	var iconAPI = '//www.google.com/s2/favicons?domain='; 
-	var supportedMethods = ['append','prepend','before','after','html'];
-	var NodeJitsuJsonP = 'https://jsonp.nodejitsu.com/?callback=?&url=';
-	var JsonObj = null; //hold reference to most recently loaded json object
+	/*** private internal constants ***/
+	var urlPartsRegX       = /^(((([^:\/#\?]+:)?(?:(\/\/)((?:(([^:@\/#\?]+)(?:\:([^:@\/#\?]+))?)@)?(([^:\/#\?\]\[]+|\[[^\/\]@#?]+\])(?:\:([0-9]+))?))?)?)?((\/?(?:[^\/\?#]+\/+)*)([^\?#]*)))?(\?[^#]+)?)(#.*)?/;
+	var iconForUrl         = '//www.google.com/s2/favicons?domain_url='; 
+	var iconForHost        = '//www.google.com/s2/favicons?domain='; 
+	var supportedMethods   = ['append','prepend','before','after','html'];
+	var jsonProxy          = 'https://jsonp.nodejitsu.com/?callback=?&url=';
+	var jsonEditorEndpoint = 'https://jsonblob.com';
+	var jsonEditorApi      = '/api/jsonBlob';
+	var	FileApiSupported   = window.File && window.FileReader && window.FileList;
 	
-	function getIconUrl ( url ) {
-		return iconAPI + regx.exec(url)[11];
-	}
-
-	function buildFromJson ( jsonData, node, settings ) {
-		var items = [];
-		var template = settings.jsonTemplate;
-		var callback = settings.onSuccess;
-		JsonObj = jsonData;
-		items.push(template.begin);
-		$.each(jsonData, function( index, value ) {
-			items.push( template.beforeEachCategory + value.category + template.afterEachCategory);
-			items.push(template.beforeLinks);
-			$.each(value.links, function(index2, site) {
-				if (template.beforeEachLink) {
-					items.push(formatSite(site, template.beforeEachLink, template));
-				}
-				if (template.eachLink) {
-					items.push(formatSite(site, template.eachLink, template));
-				}
-				if (template.afterEachLink) {
-					items.push(formatSite(site, template.afterEachLink, template));
-				}
-			});
-			items.push(template.afterLinks);
-		});
-		items.push(template.end);
-		node.html(items.join(""));
-		node.addClass(settings.addClass);
-		if ($.isFunction(callback)) {
-			callback(node);
-		}
-	}
-
-	function loadFromJsonUrl (node, settings) {
-		var url = (settings.useNodeJitsu) ? NodeJitsuJsonP + settings.json : settings.json;
-		$.getJSON( url ).done( function(data) {
-			buildFromJson(data, node, settings);
-		});
-	}
-
+	/** private internal variables */
+	var asyncHelper        = null;
 	
-	function formatSite(site, layout, opts) {
-		var url = (typeof site==='object') ? site.link : site;
-		var name = (typeof site==='object') ? site.name : regx.exec(site)[11];
-		var temp = layout.replace(opts.replaceWithIconUrl, getIconUrl(url));
-			temp = temp.replace(opts.replaceWithSiteUrl, url);
-			temp = temp.replace(opts.replaceWithSiteName, name);
-		return temp;
-	}
 	
 	/**
-	 * add DOM chaining method to global jQuery object 
-	 */
+	* add DOM chaining method to global jQuery object
+	* @param {object} options - an optional object specifying overrides of the default configuration
+	*/
 	$.fn.linkroll = function ( options ) {
-//		var roller = new LinkRoller(options);
-		var settings = $.extend({}, $.fn.linkroll.defaults, options);
-		//ensure that selected method is supported
-		settings.method = ($.inArray(settings.method, supportedMethods)) ? settings.method : supportedMethods[0];
+		var roller = new LinkRoll(options);
 		return this.each(function(){
 			var node = $(this);
-			if (settings.json) {
-				loadFromJsonUrl(node, settings);
+			roller.targetNode = node;
+			if (roller.options.jsonUrl) {
+				roller.loadFromJsonUrl(roller.options.jsonUrl);
 			} else {
 				if (node.is('a')) {
-					insertImage(node, settings);
+					roller.insertImage(node);
 				} else {
 					node.find('a').each(function (){
 						var subNode = $(this);
-						insertImage(subNode, settings);
+						roller.insertImage(subNode);
 					});
 				}
-				if ($.isFunction(settings.onSuccess)) {
-					settings.onSuccess(node);
+				if ($.isFunction(roller.options.onSuccess)) {
+					roller.options.onSuccess(node);
 				}
+			}
+			if (roller.options.buttons && roller.options.buttons.loadFromFile) {
+				node.append(roller.LoadFromFileButton());
 			}
 			return this;
 		});
 	};
 
-	function insertImage ( link, settings ) {
-		var href = $(link).attr('href');
-		var url = getIconUrl(href);
-		var img = $('<img />', {src: url});
-		img.addClass(settings.addClass);
-		link.addClass(settings.addClass);
-		var insertionMethod = link[settings.method];
-		insertionMethod.call(link, img);
-	}
-	
 	/**
-	 * default configuration is globally accessible
+	 * expose a 'read-only' copy of the supported insertion methods
+	 */
+	$.fn.linkroll.methods = function() {
+		return 	supportedMethods.slice();
+	};
+
+	/**
+	 * default configuration is globally accessible to allow overrides
 	 */
 	$.fn.linkroll.defaults = {
-		addClass: 'linkroll',  //class name to add to all created/modified elements
-		method: 'prepend',     //jQuery node insertion method for img tag
-		json  : false,         //optinal url of json to be loaded
-		useNodeJitsu: false,   //set true if json url is (trusted) remote host that does not enable CORS or JSONP
-		onSuccess : false,     //optional callback function to apply additional formatting (useful when loading json async)
+		addClass    : 'linkroll', //class name to add to all created/modified elements
+		method      : 'append',  //jQuery node insertion method for img tag (must be one of the supported methods)
+		jsonUrl     : false,      //optinal url of json to be loaded
+		useProxy    : false,      //set true if json url is (trusted) remote host that does not enable CORS or JSONP
+		onSuccess   : false,      //optional callback function to apply additional formatting (useful when loading json async)
+		buttons     : {
+			//a set of buttons that can be added to the UI when building from JSON
+			loadFromFile : false,
+			loadFromUrl  : false,
+			exportJson   : false,
+			editJson     : false,
+			reloadFromUrl: false
+		},
 		jsonTemplate: {
 			begin: '',
 			beforeEachCategory: '<h3>',
@@ -135,55 +103,152 @@
 			replaceWithSiteName: '##SITENAME##'
 		}
 	};
-	
+
+	function getIconUrl ( url ) {
+		return iconForHost + urlPartsRegX.exec(url)[11];
+	}
+
+	function formatSite ( site, layout, opts ) {
+		//allow for multiple json structures...
+		var url = (typeof site.link==='string') ? site.link : site;
+		var name = (typeof site.name==='string') ? site.name : (typeof site.link==='string') ? 	urlPartsRegX.exec(site.link)[11] : 	urlPartsRegX.exec(site)[11];
+		
+		var temp = layout.replace(opts.replaceWithIconUrl, getIconUrl(url));
+			temp = temp.replace(opts.replaceWithSiteUrl, url);
+			temp = temp.replace(opts.replaceWithSiteName, name);
+		return temp;
+	}
+
 	/**
-	 * return a 'read-only' copy of the supported insertion methods
+	 * @constructor
+	 * @param {Object} opts - overrides of $.fn.linkroll.defaults
 	 */
-	$.fn.linkroll.methods = function() {
-		return 	supportedMethods.slice();
-	};
-	
-	function LinkRoll ( opts ) {
+	function LinkRoll ( opts) {
 		this.init(opts);
+		if (this.options.jsonUrl) {
+			this.sourceUrl = this.options.jsonUrl;
+		}
 	}
 	
 	LinkRoll.prototype = {
 		options: {},
+		targetNode: null, 
+		sourceUrl: null, 
+		jsonModel: null, //hold reference to most recently loaded json object
 		init: function(overrides) {
 			this.options = $.extend({}, $.fn.linkroll.defaults, overrides);
+			//ensure that selected method is supported
+			this.options.method = ($.inArray(this.options.method, supportedMethods)) ? this.options.method : supportedMethods[0];
 		},
+		enableButtons: function ( ) {
+			this.options.buttons = {
+				loadFromFile : true,
+				loadFromUrl  : true,
+				exportJson   : true,
+				editJson     : true,
+				reloadFromUrl: true
+			};
+		},
+		disableButtons: function ( ) {
+			this.options.buttons = false;
+		},
+	
+		/**
+		 * load data from url 
+		 * @param {String} url - a json url
+		 */
+		loadFromJsonUrl: function ( url ) {
+			this.sourceUrl = (this.options.useProxy) ? jsonProxy + url : url;
+			asyncHelper = this;
+			$.getJSON( this.sourceUrl ).done( function ( data ) {
+				asyncHelper.buildFromJson(data);
+			});
+		},
+
+		buildFromJson: function  ( jsonData ) {
+			var template = this.options.jsonTemplate;
+			var callback = this.options.onSuccess;
+			this.jsonModel = jsonData;  //save json object to internal model
+			var content = [];           //temp array to build a string
+			content.push(template.begin);
+			$.each(jsonData, function( index, value ) {
+				content.push( template.beforeEachCategory + value.category + template.afterEachCategory);
+				content.push(template.beforeLinks);
+				$.each(value.links, function(index2, site) {
+					if (template.beforeEachLink) {
+						content.push(formatSite(site, template.beforeEachLink, template));
+					}
+					if (template.eachLink) {
+						content.push(formatSite(site, template.eachLink, template));
+					}
+					if (template.afterEachLink) {
+						content.push(formatSite(site, template.afterEachLink, template));
+					}
+				});
+				content.push(template.afterLinks);
+			});
+			content.push(template.end);
+			this.targetNode.html(content.join(""));
+			if (this.options.addClass) {
+				this.targetNode.addClass(this.options.addClass);
+			}
+			if ($.isFunction(callback)) {
+				callback(this.targetNode);
+			}
+		},
+
+		/**
+		 * insert an <img> into the DOM by a link using one of the supported insertion methods
+		 * @param {Object} link - an <a> element selected by jQuery
+		 */
+		insertImage: function ( link ) {
+			var href = $(link).attr('href');
+			var url = getIconUrl(href);
+			var img = $('<img />', {src: url});
+			if (this.options.addClass) {
+				img.addClass(this.options.addClass);
+				link.addClass(this.options.addClass);
+			}
+			var insertionMethod = link[this.options.method];
+			insertionMethod.call(link, img);
+		},
+		
+		LoadFromFileButton: function () {
+			var span = $('<span/>');
+			if (FileApiSupported) {
+				asyncHelper = this;
+				var input = $('<input />', {type: 'file', css: {visibility: 'hidden', width: 0, height: 0}});
+				input.attr('accept', 'json');
+				input.on('change', function(evt) {
+				    var files = evt.target.files;  // HTML5 FileList
+				    var f = files[0];              // HTML5 File
+				    var reader = new FileReader(); // HTML5 FileReader
+				    reader.onload = (function (theFile) {
+				        return function (e) { 
+				            var fileObj = e.target.result;
+				            var data = JSON.parse(fileObj);
+				            asyncHelper.buildFromJson ( data );
+				        };
+				    })(f);
+				    reader.readAsText(f, 'UTF-8');
+				});
+				var btn = $('<button/>', {type: 'button'});
+				btn.html("load from file");
+				btn.click( function () {
+					input.click();
+				});
+				span.append(btn);
+				span.append(input);
+			} else {
+				span.html("File API is not supported");
+			}
+			return span;
+		},
+		
+		
 	};
 	
-	//feature detection to see if all necessary HTML5 File APIs are supported.
-	if (window.File && window.FileReader && window.FileList) {
-		$.fn.linkroll.ImportButton = function (nodeToLoad, options) {
-			var settings = $.extend({}, $.fn.linkroll.defaults, options);
-			var input = $('<input />', {type: 'file', id: 'linkroll.input', name: 'linkroll.files[]'});
-			//based on http://jsfiddle.net/8kUYj/234/
-			input.on('change', function(evt) {
-			    var files = evt.target.files;  // HTML5 FileList
-			    var f = files[0];              // HTML5 File
-			    var reader = new FileReader(); // HTML5 FileReader
-			    reader.onload = (function (theFile) {
-			        return function (e) { 
-			            var fileObj = e.target.result;
-			            var json = JSON.parse(fileObj);
-			            buildFromJson ( json, nodeToLoad, settings );
-			        };
-			    })(f);
-			    reader.readAsText(f, 'UTF-8');
-			});
-			return input;
-		};
-	} else {
-		$.fn.linkroll.ImportButton = function() {
-			return false;
-		};
-		console.log('The File APIs are not fully supported in this browser.');
-	}
 
-	var jsonEditorEndpoint = 'https://jsonblob.com';
-	var jsonEditorApi = '/api/jsonBlob';
 	if (true) {
 		$.fn.linkroll.EditButton = function (link, options) {
 			var btn = $('<button/>',{type: 'submit', formmethod: 'GET', formtarget: '_blank'});
@@ -195,7 +260,6 @@
 					url: jsonEditorEndpoint + jsonEditorApi,
 					contentType: 'application/json; charset=utf-8',
 					type: 'POST',
-					//dataType: 'json',
 					data: jsonTxt,
 					error: function (xhr, status) {
 						alert(status);
@@ -204,25 +268,29 @@
 						console.log(data);
 						console.log(status);
 						console.log(xhr);
-						var location = xhr.getResponseHeader("location");
-						alert(location);
+						var location = xhr.getResponseHeader("location"); //
 						var editor = location.replace(jsonEditorApi, '');
 						window.open(editor);
+						btn.after(reloadButton(location));
 					}
 				});
-				// var tmp = $('<form/>', {
-				// 	method: 'POST', 
-				// 	target: "_blank",
-				// 	action: 'https://jsonblob.com/api/jsonBlob'
-				// });
-				// $("body").append(tmp);
-				// tmp.submit();
-				// tmp.remove();
 			});
 			return btn;
 		};
 	} else {
 		console.log('The File APIs are not fully supported in this browser.');
+	}
+
+	/**
+	 * get a button to reload content from the given url
+	 */
+	function reloadButton ( url ) {
+		var btn = $('<button/>');
+		btn.html('reload');
+		btn.click( function () {
+			alert(url)
+		});
+		return btn;
 	}
 
 	if (true) {
